3737ed42ef642adcd431bb08ef6f92d3985cc35c
David Kestering <davidkestering@gmail.com>
2016-04-20 15:32:09 -0300

    CursoJavaBasico

--- curso-java-basico/src/com/davidkestering/cursojava/aula15exercicios/Exerc22.java
+++ curso-java-basico/src/com/davidkestering/cursojava/aula15exercicios/Exerc22.java
@@ -8,6 +8,25 @@ import java.util.Scanner;
 public class Exerc22 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
+        System.out.println("Infome qts kg de morangos deseja.");
+        int morangokg = scan.nextInt();
+        double precoMora = 2.50;
+        if(morangokg>5)
+            precoMora = 2.20;
+        System.out.println("Informe qts kg de maças deseja.");
+        int macakg = scan.nextInt();
+        double precoMaca = 1.80;
+        if(macakg>5)
+            precoMaca = 1.50;
+
+        int totalKg = morangokg+macakg;
+        double valorCompras = (precoMora*morangokg)+(precoMaca*macakg);
+        double valorTotal = valorCompras;
+        //System.out.println("Valor das Compras "+valorCompras);
+        //System.out.println("Valor sem desconto "+valorTotal);
+        if(totalKg > 8 || valorCompras > 25)
+            valorTotal = valorCompras-(valorCompras*10/100);
+        System.out.println("O valor total a ser pago é R$ "+valorTotal);
+
     }
 }
--- curso-java-basico/src/com/davidkestering/cursojava/aula15exercicios/Exerc23.java
+++ curso-java-basico/src/com/davidkestering/cursojava/aula15exercicios/Exerc23.java
@@ -8,5 +8,45 @@ import java.util.Scanner;
 public class Exerc23 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("Informe o tipo de carne que deseja: (F/A/P)");
+        String tipo = scan.next();
+        System.out.println("Informe qts kg deseja comprar.");
+        int kg = scan.nextInt();
+        System.out.println("Informe a forma de pagamento. (C/D)");
+        String formaPag = scan.next();
+
+        int descontoPag = 0;
+        if(formaPag.equalsIgnoreCase("c"))
+            descontoPag = 5;
+
+        double preco = 0;
+        switch (tipo){
+            case "F":
+                preco = 4.90;
+                if(kg>5)
+                    preco = 5.80;
+                break;
+            case "A":
+                preco = 5.90;
+                if(kg>5)
+                    preco = 6.80;
+                break;
+            case "P":
+                preco = 6.90;
+                if(kg>5)
+                    preco = 7.80;
+                break;
+        }
+
+        double valorDesconto = (preco*descontoPag/100);
+        //System.out.println(valorDesconto);
+        double valorTotal = (preco-valorDesconto)*kg;
+        System.out.println("Você escolheu o tipo de carne "+tipo+"\n"+
+                            "Você escolheu a qtde de "+kg+"kg\n"+
+                            "O preço total é de "+(valorTotal+valorDesconto)+"\n"+
+                            "O tipo de pagamento é "+formaPag+"\n"+
+                            "O valor de desconto é de "+valorDesconto+"\n"+
+                            "O valor a pagar é de "+valorTotal);
+
     }
 }
